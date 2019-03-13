52d99f04ea90f41a7b10ed2bb21274732f2f13a5
Mintaka <spaceisperfect@gmail.com>
2019-03-13 21:32:20 +0300

    Add basic logic.

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Application
+{
+    using AsyncAwaitWorkshop;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PageLoader loader = new PageLoader("https://vk.com/", "C:\\Users\\space\\Desktop\\disa");
+            Task.WaitAll(loader.GetAsync());
+        }
+    }
+}
--- AsyncAwaitWorkshop/PageLoader.cs
+++ AsyncAwaitWorkshop/PageLoader.cs
@@ -1,7 +1,117 @@
 ﻿namespace AsyncAwaitWorkshop
 {
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using CsQuery;
+
+    using MoreLinq;
+
     public class PageLoader
     {
-        
+        private readonly Uri baseUrl;
+
+        private readonly DirectoryInfo directory;
+
+        private readonly PageLoaderSettings settings;
+
+        public PageLoader(string startFrom, string saveTo)
+        {
+            this.baseUrl = new Uri(startFrom);
+            this.directory = new DirectoryInfo(saveTo);
+            if (!this.directory.Exists)
+            {
+                this.directory.Create();
+            }
+        }
+
+        public PageLoader(string startFrom, string saveTo, PageLoaderSettings settings)
+            : this(startFrom, saveTo)
+        {
+            this.settings = settings;
+        }
+
+        public async Task GetAsync()
+        {
+            await this.GetAsync(this.baseUrl);
+        }
+
+        private async Task GetAsync(Uri uri)
+        {
+            if (uri == null)
+            {
+                return;
+            }
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(uri))
+            using (HttpContent content = response.Content)
+            {
+                string result = await content.ReadAsStringAsync();
+                if (result != null)
+                {
+                    using (var streamWriter = File.CreateText($"{this.directory.FullName}\\" +
+                                                              DateTime.Now.ToString("yyyyMMddHHmmss") +
+                                                              $"{DateTime.Now.Millisecond}.html"))
+                    {
+                        await streamWriter.WriteAsync(result);
+                    }
+
+                    var cq = CQ.CreateDocument(result);
+                    foreach (var c in cq.Find("a").Each(c =>
+                        {
+                            if (c.GetAttribute("href") == null)
+                            {
+                                return;
+                            }
+
+                            var index = c.GetAttribute("href").IndexOf('#');
+                            if (index != -1)
+                            {
+                                c.SetAttribute("href", c.GetAttribute("href").Substring(0, index));
+                            }
+                        }).DistinctBy(c => c.GetAttribute("href")))
+                    {
+                        var newUri = this.UriFromString(uri, c.GetAttribute("href"));
+                        if (newUri == null || newUri.AbsolutePath == uri.AbsolutePath)
+                        {
+                            continue;
+                        }
+                        
+                        await this.GetAsync(newUri);
+                    }
+                }
+            }
+        }
+
+        private Uri UriFromString(Uri uriFrom, string link)
+        {
+            if (link == null)
+            {
+                return null;
+            }
+
+            if (Uri.IsWellFormedUriString(link, UriKind.Absolute))
+            {
+                return new Uri(link);
+            }
+
+            if (link.StartsWith("//"))
+            {
+                return new Uri($"{uriFrom.Scheme}:{link}");
+            }
+
+            if (link.StartsWith("/"))
+            {
+                return new Uri(uriFrom, link.Remove(0, 1));
+            }
+
+            return null;
+        }
     }
 }
